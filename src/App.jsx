f41bff177392ba24be126f5ca78757d1a6a01c62
GaurAdarsh <gaur95645@gmail.com>
2025-10-07 21:31:38 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Signin from './pages/Signin'
 import Profile from './pages/Profile'
 import SignUp from './pages/SignUp'
+import Header from './assets/components/header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <BrowserRouter> 
+   <Header/>
    <Routes>
    <Route path='/' element={<Home/>}/> 
    <Route path='/about' element={<About/>}/>  
